03bd390436167716a66f2c69a660282eb3b1bed9
Daniel_praca <danielkaleta97@gmail.com>
2020-06-23 20:06:29 +0200

    Rozszerzono funkcjonalność klawiatury o "Pierwsza wielka, kolejne małe"

--- Raspberry QT/Keyboard.cpp
+++ Raspberry QT/Keyboard.cpp
@@ -28,11 +28,21 @@ void Keyboard::activate(QLineEdit *toEdit)
         this->type = 0;
         on_NUMERYCZNA_clicked();
     }
+    else if(toEdit->text().size()==0)
+    {
+        this->type = 2;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_Q_clicked()
 {
     toEdit->insert(ui->Q->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_W_clicked()
@@ -48,86 +58,177 @@ void Keyboard::on_W_clicked()
 void Keyboard::on_E_clicked()
 {
     toEdit->insert(ui->E->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_R_clicked()
 {
     toEdit->insert(ui->R->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_T_clicked()
 {
     toEdit->insert(ui->T->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_Y_clicked()
 {
     toEdit->insert(ui->Y->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_U_clicked()
 {
     toEdit->insert(ui->U->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_I_clicked()
 {
     toEdit->insert(ui->I->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_O_clicked()
 {
     toEdit->insert(ui->O->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_P_clicked()
 {
     toEdit->insert(ui->P->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_A_clicked()
 {
     toEdit->insert(ui->A->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_S_clicked()
 {
     toEdit->insert(ui->S->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_D_clicked()
 {
-    toEdit->insert(ui->D->text());
+    QString text = ui->D->text();
+    if(text.compare("&&") == 0)
+    {
+        text = "&";
+    }
+    toEdit->insert(text);
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
+
 }
 
 void Keyboard::on_F_clicked()
 {
     toEdit->insert(ui->F->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_G_clicked()
 {
     toEdit->insert(ui->G->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_H_clicked()
 {
     toEdit->insert(ui->H->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_J_clicked()
 {
     toEdit->insert(ui->J->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_K_clicked()
 {
     toEdit->insert(ui->K->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_L_clicked()
 {
     toEdit->insert(ui->L->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_CAPS_clicked()
@@ -139,36 +240,71 @@ void Keyboard::on_CAPS_clicked()
 void Keyboard::on_Z_clicked()
 {
     toEdit->insert(ui->Z->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_X_clicked()
 {
     toEdit->insert(ui->X->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_C_clicked()
 {
     toEdit->insert(ui->C->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_V_clicked()
 {
     toEdit->insert(ui->V->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_B_clicked()
 {
     toEdit->insert(ui->B->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_N_clicked()
 {
     toEdit->insert(ui->N->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_M_clicked()
 {
     toEdit->insert(ui->M->text());
+    if(toEdit->text().size()==1)
+    {
+        this->type = 0;
+        on_NUMERYCZNA_clicked();
+    }
 }
 
 void Keyboard::on_BACKSPACE_clicked()
@@ -292,12 +428,7 @@ void Keyboard::on_NUMERYCZNA_clicked()
 
 void Keyboard::on_PRZECINEK_clicked()
 {
-    QString text = ui->PRZECINEK->text();
-    if(text.compare("&&"))
-    {
-        text = "&";
-    }
-    toEdit->insert(text);
+    toEdit->insert(ui->PRZECINEK->text());
 }
 
 void Keyboard::on_SPACE_clicked()

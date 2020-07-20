e8aab1a5b80836380b3dddf192f0a7db0313c25e
Daniel Kaleta <danielkaleta97@gmail.com>
2020-07-20 13:49:06 +0200

    kolejnePoprawki

---
+++ kolejnePoprawki/RegistrationScreen.cpp
@@ -0,0 +1,227 @@
+#include "RegistrationScreen.h"
+#include "ui_RegistrationScreen.h"
+#include "QMouseEvent"
+#include "LoggedScreen.h"
+#include "TakePictureScreen.h"
+
+RegistrationScreen::RegistrationScreen(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::RegistrationScreen)
+{
+    ui->setupUi(this);
+    setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+    keyboard = keyboard->getKeyboard();
+    keyboard->move(0,388); //418
+
+    ui->imie->installEventFilter(this);
+    ui->nazwisko->installEventFilter(this);
+    ui->email->installEventFilter(this);
+    ui->haslo->installEventFilter(this);
+    ui->powtHaslo->installEventFilter(this);
+    ui->nazwiskoLabel->installEventFilter(this);
+
+    kodokanDAO = kodokanDAO->getInstance();
+}
+
+RegistrationScreen::~RegistrationScreen()
+{
+    delete ui;
+}
+
+bool RegistrationScreen::eventFilter(QObject *obj, QEvent *event)
+{
+    if( (obj == ui->haslo || obj == ui->powtHaslo) && event->type() == QEvent::FocusIn)
+    {
+        ui->nazwisko->hide();
+        ui->nazwiskoLabel->hide();
+        ui->imie->hide();
+        ui->imieLabel->hide();
+        ui->email->hide();
+        ui->emailLabel->hide();
+
+        ui->haslo->setGeometry(359,2,508,100);
+        ui->hasloLabel->setGeometry(153,2,200,100);
+        ui->powtHaslo->setGeometry(359,92,508,100);
+        ui->powtHasloLabel->setGeometry(153,92,508,100);
+
+        if(obj == ui->haslo)
+        {
+            ui->haslo->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(200, 200, 200);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+            ui->powtHaslo->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+            keyboard->activate(0, ui->haslo, ui->pushButton);
+        }
+        else
+        {
+            ui->powtHaslo->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(200, 200, 200);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+            ui->haslo->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+            keyboard->activate(0, ui->powtHaslo, ui->pushButton);
+        }
+        keyboard->show();
+        keyboard->activateWindow();
+    }
+    else if( (obj == ui->nazwisko || obj == ui->email || obj == ui->imie || obj == ui->nazwiskoLabel) && event->type() == QEvent::FocusIn)
+    {
+        ui->nazwisko->show();
+        ui->nazwiskoLabel->show();
+        ui->email->show();
+        ui->emailLabel->show();
+        ui->haslo->show();
+        ui->hasloLabel->show();
+        ui->imie->show();
+        ui->imieLabel->show();
+
+        ui->email->setGeometry(359,182,508,100);
+        ui->emailLabel->setGeometry(153,182,200,100);
+        ui->haslo->setGeometry(359,272,508,100);
+        ui->hasloLabel->setGeometry(153,272,200,100);
+        ui->powtHaslo->setGeometry(359,362,508,100);
+        ui->powtHasloLabel->setGeometry(153,362,200,100);
+
+        if( obj == ui->imie)
+        {
+            ui->imie->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(200, 200, 200);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+            ui->nazwisko->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+            ui->email->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+            keyboard->activate(2, ui->imie, ui->pushButton);
+            keyboard->show();
+            keyboard->activateWindow();
+        }
+        else if(obj == ui->nazwisko)
+        {
+            ui->nazwisko->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(200, 200, 200);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+            ui->imie->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+            ui->email->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+            keyboard->activate(2, ui->nazwisko, ui->pushButton);
+            keyboard->show();
+            keyboard->activateWindow();
+        }
+        else if(obj == ui->email)
+        {
+            ui->email->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(200, 200, 200);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+            ui->nazwisko->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+            ui->imie->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+            keyboard->activate(0, ui->email, ui->pushButton);
+            keyboard->show();
+            keyboard->activateWindow();
+        }
+    }
+    return false;
+}
+
+void RegistrationScreen::on_powrot_clicked()
+{
+    ui->imie->setText("");
+	ui->imie->clear();
+    ui->nazwisko->setText("");
+	ui->nazwisko->clear();
+    ui->haslo->setText("");
+	ui->haslo->clear();
+    ui->powtHaslo->setText("");
+	ui->powtHaslo->clear();
+    ui->email->setText("");
+	ui->email->clear();
+    keyboard->hide();
+    this->close();
+}
+
+void RegistrationScreen::mousePressEvent(QMouseEvent *event)
+{
+    ui->imie->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+    ui->nazwisko->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+    ui->email->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+    ui->haslo->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+    ui->powtHaslo->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);font: 75 30pt \"Tahoma\";border-style: solid;border-width:4px;border-radius:30px;");
+    ui->nazwiskoLabel->show();
+    ui->nazwiskoLabel->setFocus();
+    keyboard->hide();
+}
+
+void RegistrationScreen::on_pushButton_clicked()
+{
+    QString imie = ui->imie->text();
+    QString nazwisko = ui->nazwisko->text();
+    QString email = ui->email->text();
+    QString haslo = ui->haslo->text();
+    QString powtHaslo = ui->powtHaslo->text();
+
+    //Sprawdzanie czy jakies pole jest puste
+    if(imie.isEmpty())
+    {
+        registrationInfoScreen = new RegistrationInfoScreen(nullptr, "IMIĘ NIE MOŻE BYĆ PUSTE");
+        registrationInfoScreen->move(162,234);
+        registrationInfoScreen->show();
+        return;
+    }
+    if(nazwisko.isEmpty())
+    {
+        registrationInfoScreen = new RegistrationInfoScreen(nullptr, "NAZWISKO NIE MOŻE BYĆ PUSTE");
+        registrationInfoScreen->move(162,234);
+        registrationInfoScreen->show();
+        return;
+    }
+    if(email.isEmpty())
+    {
+        registrationInfoScreen = new RegistrationInfoScreen(nullptr, "EMAIL NIE MOŻE BYĆ PUSTY");
+        registrationInfoScreen->move(162,234);
+        registrationInfoScreen->show();
+        return;
+    }
+    if(haslo.isEmpty())
+    {
+        registrationInfoScreen = new RegistrationInfoScreen(nullptr, "HASŁO NIE MOŻE BYĆ PUSTE");
+        registrationInfoScreen->move(162,234);
+        registrationInfoScreen->show();
+        return;
+    }
+    //Sprawdzanie, czy powtorzone haslo jest takie samo
+    if(haslo.compare(powtHaslo))
+    {
+        registrationInfoScreen = new RegistrationInfoScreen(nullptr, "HASŁA NIE SĄ IDENTYCZNE");
+        registrationInfoScreen->move(162,234);
+        registrationInfoScreen->show();
+        return;
+    }
+
+    kodokanDAO->add_user(email.toStdString(), email.toStdString(), imie.toStdString(), nazwisko.toStdString(), haslo.toStdString());
+
+    registrationInfoScreen = new RegistrationInfoScreen(nullptr, "ZAREJESTROWANO POMYŚLNIE");
+    registrationInfoScreen->move(162,234);
+    registrationInfoScreen->exec();
+
+    while(registrationInfoScreen->isActiveWindow())
+    {
+
+    }
+
+    UserDAO * loggedUser = kodokanDAO->authorize_user(email.toStdString(), haslo.toStdString());
+	
+	//Kasowanie wcześniej wypełnionych danych (ponowne btw)
+	ui->imie->setText("");
+	ui->imie->clear();
+    ui->nazwisko->setText("");
+	ui->nazwisko->clear();
+    ui->haslo->setText("");
+	ui->haslo->clear();
+    ui->powtHaslo->setText("");
+	ui->powtHaslo->clear();
+    ui->email->setText("");
+	ui->email->clear();
+
+    LoggedScreen *loggedScreen = new LoggedScreen(this,loggedUser);
+    loggedScreen->move(0,0);
+    loggedScreen->show();
+    on_powrot_clicked();
+}
+
+void RegistrationScreen::on_dodajZdjecie_clicked()
+{
+    if(takePictureScreen == nullptr)
+        takePictureScreen = new TakePictureScreen(this, ui->email->text().toStdString());
+    else
+    {
+        delete takePictureScreen;
+        takePictureScreen = new TakePictureScreen(this, ui->email->text().toStdString());
+    }
+    takePictureScreen->move(0,0);
+    takePictureScreen->show();
+}
---
+++ kolejnePoprawki/mainwindow.cpp
@@ -0,0 +1,148 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "LoginScreen.h"
+#include "LoopThread.h"
+#include <string>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+    this->threadRun=true;
+    loopThread = new LoopThread(threadRun);
+    connect( loopThread, SIGNAL(przylozonoKarte(QString)), this, SLOT(przylozonoKarte(QString)));
+    this->installEventFilter(this);
+    kodokanDAO = kodokanDAO->getInstance();
+
+    //#1.3
+    //this->setCursor(Qt::BlankCursor);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_kupKarnetButton_clicked()
+{
+    qDebug() << "Stop watek";
+    threadRun = false;
+    if(buyingPassScreen == nullptr)
+    {
+        buyingPassScreen = new BuyingPassScreen(nullptr,nullptr);
+        buyingPassScreen->move(0,0);
+        buyingPassScreen->show();
+    }
+    else
+    {
+        buyingPassScreen->show();
+		buyingPassScreen->activateWindow();
+		buyingPassScreen->raise()
+    }
+}
+
+void MainWindow::on_zalogujSieButton_clicked()
+{
+    qDebug() << "Stop watek";
+    threadRun = false;
+    if(loginScreen == nullptr)
+    {
+        loginScreen = new LoginScreen();
+        loginScreen->move(0,0);
+        loginScreen->show();
+    }
+    else
+    {
+        loginScreen->show();
+		loginScreen->activateWindow();
+		loginScreen->raise()
+    }
+}
+
+void MainWindow::on_zalozKontoButton_clicked()
+{
+    qDebug() << "Stop watek";
+    threadRun = false;
+    if(registrationScreen == nullptr)
+    {
+        registrationScreen = new RegistrationScreen();
+        registrationScreen->move(0,0);
+        registrationScreen->show();
+    }
+    else
+    {
+        registrationScreen->show();
+		registrationScreen->activateWindow();
+		registrationScreen->raise()
+    }
+}
+
+void MainWindow::on_administracja_clicked()
+{
+    qDebug() << "Stop watek";
+    threadRun = false;
+    if(administrationScreen == nullptr)
+    {
+        administrationScreen = new AdministrationScreen();
+        administrationScreen->move(0,0);
+        administrationScreen->show();
+    }
+    else
+    {
+        administrationScreen->show();
+		administrationScreen->activateWindow();
+		administrationScreen->raise()
+    }
+}
+
+bool MainWindow::eventFilter(QObject *obj, QEvent *event)
+{
+
+    if(event->type() == QEvent::WindowActivate)
+    {
+        if(!loopThread->isRunning())
+        {
+            qDebug() << "Start watek";
+            this->threadRun = true;
+            loopThread->start();
+
+        }
+    }
+    return false;
+}
+
+void MainWindow::przylozonoKarte(QString rfid)
+{
+    //B0070AFEB1
+    if(rfid.compare("B0070AFEB1") == 0)
+    {
+        qDebug() << "Stop watek";
+        threadRun = false;
+        if(administrationScreen == nullptr)
+        {
+            administrationScreen = new AdministrationScreen();
+            administrationScreen->move(0,0);
+            administrationScreen->show();
+        }
+        else
+        {
+            administrationScreen->show();
+			administrationScreen->activateWindow();
+			administrationScreen->raise()
+        }
+    }
+    qDebug() << rfid;
+    UserDAO *loggedUser = kodokanDAO->authorize_user(rfid.toStdString());
+    qDebug() << "Stop watek";
+    threadRun = false;
+    if(loggedUser != nullptr)
+    {
+        LoggedScreen *loggedScreen = new LoggedScreen(nullptr,loggedUser);
+        loggedScreen->move(0,0);
+        loggedScreen->show();
+		loggedScreen->activateWindow();
+		loggedScreen->raise()
+    }
+}
